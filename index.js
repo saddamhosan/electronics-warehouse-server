e938972d49db72eeb21af1cbfde5daf7104806f3
saddamhosan <contact.saddamhosan.com>
2022-04-29 00:37:01 +0600

    added mongodb

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ const port=process.env.PORT||4000
 app.use(cors())
 app.use(express.json())
 
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.arfbr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log('db connected');
+  // perform actions on the collection object
+  client.close();
+});
+
+
 app.get('/',(req,res)=>{
     res.send('welcome to electronics warehouse server')
 })
