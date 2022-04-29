11b32c63f72887a3034cc403d4a38dafd99c7250
saddamhosan <contact.saddamhosan.com>
2022-04-29 16:42:25 +0600

    added get single product

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.arfbr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-console.log(uri);
 //heroku link https://enigmatic-beach-29740.herokuapp.com/
 
 async function run() {
@@ -28,6 +27,13 @@ async function run() {
       const query={}
       const cursor=productsCollection.find(query)
       const result=await cursor.toArray()
+      res.send(result)
+    })
+
+    app.get('/product/:id',async(req,res)=>{
+      const id=req.params.id
+      const query={_id:ObjectId(id)}
+      const result=await productsCollection.findOne(query)
       res.send(result)
     })
     
