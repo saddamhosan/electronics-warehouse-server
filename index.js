9829ac748b22c50988ba7945c38a945ca33d9708
saddamhosan <contact.saddamhosan.com>
2022-04-29 21:34:05 +0600

    added update link

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const app=express()
-const port=process.env.PORT||4000
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 4000;
 
 //middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const res = require("express/lib/response");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.arfbr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -22,20 +22,36 @@ async function run() {
   try {
     await client.connect();
     const productsCollection = client.db("warehouse").collection("products");
-    
-    app.get('/products',async(req,res)=>{
-      const query={}
-      const cursor=productsCollection.find(query)
-      const result=await cursor.toArray()
-      res.send(result)
-    })
 
-    app.get('/product/:id',async(req,res)=>{
-      const id=req.params.id
-      const query={_id:ObjectId(id)}
-      const result=await productsCollection.findOne(query)
-      res.send(result)
-    })
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.put("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const quantity = parseInt(req.body.newQuantity);
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: { quantity },
+      };
+      const result = await productsCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
     
   } finally {
     // await client.close();
@@ -43,11 +59,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("welcome to electronics warehouse server");
+});
 
-app.get('/',(req,res)=>{
-    res.send('welcome to electronics warehouse server')
-})
-
-app.listen(port,()=>{
-    console.log('Listing to port',port);
-})
+app.listen(port, () => {
+  console.log("Listing to port", port);
+});
